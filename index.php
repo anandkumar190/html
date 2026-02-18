8043c24c343f3a37d2b55e6ff1c7bbd44bf1c981
Anand <anandkumar.kumar190@gmail.com>
2026-02-18 21:04:45 +0530

    home page not working .

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
   session_start();
+
+  ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
    if(!isset($_SESSION['tittu']))
    {
 	   header("loaction:login");
@@ -9,8 +14,7 @@
    }
    else
    {
-	  // header("loaction:home");
-     header("Location: /html/home/");
+	   header("loaction:home");
 	  //  echo"<script>window.location='home';</script>";
 	   exit();
    }
