7d4689237a4203e4127eb2047106e64c4490d95f
Anand <anandkumar.kumar190@gmail.com>
2026-02-25 16:21:09 +0530

    debug activity

--- employee-report-of-selected-period.php
+++ employee-report-of-selected-period.php
@@ -1,4 +1,7 @@
 <?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 
     function getDatesFromRange($start, $end, $format = 'Y-m-d') { 
       
