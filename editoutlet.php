e7228fa4ec366eac9e28f4edd79af0e2aaaf7ef0
Anand <anandkumar.kumar190@gmail.com>
2026-03-24 19:34:02 +0530

    updated distributor

--- editoutlet.php
+++ editoutlet.php
@@ -98,10 +98,6 @@
 		
 	}
   }
-
-
-  $outletType=["MTS","G.T.","MTL","Milk Both"];
-
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -175,19 +171,7 @@
                 </div>
                 <div class="form-group">
                   <label for="outlettype">Outlet Type : </label>
-
-
-              
-                  <select  class="form-control select2" name="outlettype" id="outlettype" required>
-                       <option value=" ">Select Type</option>
-                   <?php 
-                   foreach ($outletType as $i => $type) {
-                          $isSeleted=$type==$outlettype?"selected":"";
-                    echo "  <option  ".$isSeleted." value=".$type." >".$type."</option>";
-                   }
-                   ?>
-                
-            
+                  <input type="text" class="form-control" name="outlettype" id="outlettype" value="<?php echo $outlettype;?>" placeholder="OutletType" required />
                 </div>
                 
                 <!-- <div class="form-group">
