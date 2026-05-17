d820fa3956865700d475756e15d51b9ada27b1be
Anand <anandkumar.kumar190@gmail.com>
2026-05-17 22:05:12 +0530

    testmail for mail

--- api/text-mail.php
+++ api/text-mail.php
@@ -8,15 +8,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require __DIR__ . '/../vendor/autoload.php';
-require("connect.php");
+//require("connect.php");
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 /*
 |--------------------------------------------------------------------------
